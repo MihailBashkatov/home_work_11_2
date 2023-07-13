a0114c42023d2af70d054b90295e3ec1e383f699
Mihail Bashkatov <mihail.bashkatov@gmail.com>
2023-07-13 20:36:43 +0200

    Added new func get_capital_words and changed docstring for func get_capital

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def get_capital(letters: str):
-    """Return capital letters"""
+    """Return capital letters for each letter"""
     return letters.upper()
+
+
+def get_capital_words(words: str):
+    """Return first capital letters for each word"""
+    return words.title()
