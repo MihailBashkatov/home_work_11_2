3230faa2c12680cf8831a3530b9c79c03e994490
Mihail Bashkatov <mihail.bashkatov@gmail.com>
2023-07-13 16:39:40 +0200

    Added func get_capital_word and changing docstring for func get_capital

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def get_capital(letters: str):
-    """Return capital letters"""
+    """Возвращает строку с о всеми заглавными буквами"""
     return letters.upper()
+
+
+def get_capital_word(words: str):
+    """Возвращает строку с первыми заглавными буквами строк"""
+    return words.upper()
